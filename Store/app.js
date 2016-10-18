578e7380122fc705dbaac348dd4e40f73e3d6a8f
Devorah Langsam <devorah@langsam.org>
2016-10-18 09:47:59 -0400

    Doing codeschool tutorial

---
+++ Store/app.js
@@ -0,0 +1,9 @@
+(function(){
+  
+  var app = angular.module('store', []);
+  app.controller('StoreController', function(){
+    this.product = gem; 
+  });
+  var gem = { name: 'Azurite', price: 2.95,
+  description: "Beautiful cut and color", canPurchase: true };
+})();
