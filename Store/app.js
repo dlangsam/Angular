c35739f3179ef1594687c196c207ae761fb5e6ad
Devorah Langsam <devorah@langsam.org>
2016-10-18 10:03:12 -0400

    Add bootstrap

--- Store/app.js
+++ Store/app.js
@@ -2,9 +2,21 @@
   
   var app = angular.module('store', []);
   app.controller('StoreController', function(){
-    this.product = gem; 
+    this.products = gems; 
   });
-  var gem = { name: 'Azurite', price: 2.95,
-  description: "Beautiful cut and color", canPurchase: true,
-  soldOut: true };
+  var gems = 
+
+
+  [
+ 	 { name: 'Azurite', price: 2.95,
+  		description: "Beautiful cut and color", 
+  		canPurchase: true
+  		},
+	 { name: 'Diamond', price: 2000.95,
+  		description: "Princess cut. Half carat", 
+  		canPurchase: true
+  		 }
+
+
+  ];
 })();
